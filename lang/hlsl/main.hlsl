ef56105a15f451c40b394fc5ebcfa9fcd6789d82
playfairs <root@playfairs.cc>
2026-03-12 15:15:37 -0500

    add: images

--- lang/hlsl/main.hlsl
+++ lang/hlsl/main.hlsl
@@ -1,5 +1,5 @@
 float4 mainPS(float4 position : SV_Position) : SV_Target
 {
-    return float4(1.0, 1.0, 1.0, 1.0); // Just white, you can change this to anything you want
+    return float4(1.0, 1.0, 1.0, 1.0);
 }
 .
